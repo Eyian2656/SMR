8d1e2b07341dcc86c40dc516027dbd86fd4282e2
Eyian2656 <Ian.Noack@outlook.de>
2017-03-27 13:57:10 +0200

    Klasse für das erstellen und beschreiben des Updateskript erstellt.

---
+++ src/logic/Skriptwriter.java
@@ -0,0 +1,55 @@
+package logic;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Skriptwriter {
+
+
+
+
+	public Skriptwriter(){
+		
+	}
+	//TODO die Syso wegmachen , else statment löschen 
+	public void findSkript() throws IOException{
+		Path path = Paths.get("C:/Users/Ian/Test/file.sql");
+		if(Files.notExists(path)){
+			this.createScript();
+		}
+		else{
+			System.out.println("File exestiert schon");
+		}
+	}
+	
+	public void createScript() throws IOException {
+		File file = new File("C:/Users/Ian/Test/file.sql");
+		file.createNewFile();
+		System.out.println("File wurde erstellt");
+	}
+	
+	
+	public void writeScript(String skriptCmd) {
+		try {
+			FileWriter outputStream = new FileWriter("C:/Users/Ian/Test/file.sql", true);
+			BufferedWriter bw = new BufferedWriter(outputStream);
+			String n = System.getProperty("line.separator");
+			
+			bw.flush();
+			bw.write(n);
+			bw.write(skriptCmd);
+			bw.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+}
+
+}
