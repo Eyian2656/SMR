064dc471dba42a1cd7335c2b4baabb930430121d
Dev <Dev@Dev-PC-Master>
2017-03-28 17:10:37 +0200

    Als nächtes sthe Rowset Implementierung an

---
+++ src/logic/DataComparer.java
@@ -0,0 +1,7 @@
+package logic;
+
+public class DataComparer {
+
+	int rowNumber;
+	
+}
---
+++ src/model/Data.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Data {
+
+	int rowNumber;
+	String data;
+	String columnName;
+
+	// Getters & Setters \\
+	public int getRowNumber() {
+		return rowNumber;
+	}
+	public void setRowNumber(int rowNumber) {
+		this.rowNumber = rowNumber;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public String getColumnName() {
+		return columnName;
+	}
+	public void setColumnName(String columnName) {
+		this.columnName = columnName;
+	}
+
+}
