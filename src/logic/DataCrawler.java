591155b5251c43ce5ee0bf337d9dc3d59b968264
Dev <Dev@Dev-PC-Master>
2017-03-31 23:41:51 +0200

    Ians Last try

--- src/logic/DataCrawler.java
+++ src/logic/DataCrawler.java
@@ -4,12 +4,13 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 
-import model.Column;
 import model.Data;
 
 /**
@@ -19,33 +20,34 @@ import model.Data;
  *
  */
 public class DataCrawler {
+	DateFormat df = new SimpleDateFormat("dd-mmm-yyyy");
 	
 	public List<Data> crawlData(Connection conn, String columnName, String tablename, String datatype) throws SQLException{
 		List<Data> dataInsideTable = new ArrayList<Data>();
 		Statement stmt = conn.createStatement();
-		String sqlStmt = "SELECT NR, " +  columnName + " FROM " + tablename;
+		String sqlStmt = "SELECT NR, NAME " +  " FROM " + tablename;
 		ResultSet rs = stmt.executeQuery(sqlStmt);
 		
 		
 		
 		while(rs.next()){
-			
+			System.out.println(rs.getInt(1) + " , "+ rs.getString(2));
 			Data tableData = new Data();
-			tableData.setNr((Integer) rs.getObject(0));
+			tableData.setNr( rs.getBigDecimal(1).intValueExact());
 
 			// Überprüfung des Datentyps 
-			if (StringUtils.equals(datatype, "NUMBER")) {
-				tableData.setValue( rs.getInt(1));
-			}
+//			if (StringUtils.equals(datatype, "NUMBER")) {
+//				tableData.setValue( String.valueOf(rs.getInt(2)));
+//			}
 			if (StringUtils.equals(datatype, "VARCHAR2")) {
-				tableData.setValue( rs.getString(1));
+				tableData.setValue( rs.getString(2));
 			}
 			if (StringUtils.equals(datatype, "DATE")) {
-				tableData.setValue( rs.getDate(1));
+				tableData.setValue( df.format(rs.getDate(2)));
 			}	
 
 				tableData.setColumnName(columnName);
-				System.out.println(tableData.getColumnName() + " = " +  tableData.getNr() + " = " + tableData.getValue());
+				//ystem.out.println(tableData.getColumnName() + " = " +  tableData.getNr() + " = " + tableData.getValue());
 				dataInsideTable.add(tableData);
 				
 		
